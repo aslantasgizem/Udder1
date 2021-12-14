5f7bd4cc139e178d6cfdcf5287eb2c4bb82767bf
aslantasgizem <aslantasgizem@gmail.com>
2021-12-14 21:03:07 +0300

    I am label with name Udder Label.

--- Udder1/ViewController.swift
+++ Udder1/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelUdder: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func button(_ sender: Any) {
+        labelUdder.text = "Hello, I am label with name Udder Label."
+    }
+    
 }
 
